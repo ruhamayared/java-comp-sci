e5bf7962eb20ef59aa684265ce263c2716965c50
Ruhama Yared <ruhamayared1@gmail.com>
2023-06-29 21:56:12 -0400

    update Course Program

--- CourseTest.java
+++ CourseTest.java
@@ -2,12 +2,17 @@ public class CourseTest {
 
     public static void main(String[] args) {
 
-        Course algbera2 = new Course(); // Creating an instance of the Course class
+        Course algbera2 = new Course("Math"); // Creating an instance of the Course class
 
-        algbera2.setName("Algebra 2"); // Calling the setName method on the algebra2 object and passing it the string
-                                       // "Algebra 2" as an argument
+        Course geometry = new Course(algbera2.getName()); // Creating an instance of the Course class
 
-        algbera2.printName(); // Calling the printName method on the algebra2 object
+        // algbera2.printName(); // Calling the printName method on the algebra2 object
+
+        // algbera2.setName("Algebra 2"); // Calling the setName method on the algebra2
+        // object and passing it the string
+        // // "Algebra 2" as an argument
+
+        geometry.printName(); // Calling the printName method on the algebra2 object
 
     }
 
